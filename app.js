e8474eda7449f827f428e61c208519fd71a14795
codd1e <andreinik32@gmail.com>
2023-03-09 16:28:10 +0300

    fix cors 2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5000"],
+    origin: ["http://localhost:3000"],
     credentials: true,
     methods: ["POST", "GET", "PATCH", "DELETE"],
     allowedHeaders: ['Content-Type','Authorization', 'contenttype'],
