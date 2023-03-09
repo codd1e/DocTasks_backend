6d4a447bf28a642a044a1e4d7b569057c571632e
codd1e <andreinik32@gmail.com>
2023-03-09 11:26:17 +0300

    fix cors

--- app.js
+++ app.js
@@ -16,6 +16,12 @@ app.use(cors({
     ],
     credentials: true
 }))
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 app.use(authRouter)
 
 const start = async () => {
