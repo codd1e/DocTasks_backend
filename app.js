fddc5bd33bdc3b9a084f32989b749da759594147
codd1e <andreinik32@gmail.com>
2023-03-10 12:43:23 +0300

    change cors

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:3000/sign"],
-    credentials: true,
-    methods: ["POST", "GET", "PATCH", "DELETE"],
-    allowedHeaders: ['Content-Type','Authorization', 'contenttype'],
-    exposedHeaders: []
-}))
+app.use(cors())
 app.use(authRouter)
 
 const start = async () => {
