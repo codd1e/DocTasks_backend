1b3afe3be2b1c244f5cdaddccd328897a421561c
codd1e <andreinik32@gmail.com>
2023-03-10 13:51:17 +0300

    change cors 2

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: ['http://localhost:3000', 'http://localhost:3000/sign', 'https://doc-tasks-front.vercel.app', 'https://doc-tasks-front.vercel.app/sign']
+    methods: ['POST', 'GET', 'PUT'],
 }))
 app.use(authRouter)
 
